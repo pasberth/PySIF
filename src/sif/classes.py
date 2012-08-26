93d651958fe2ea3ba988e42ff15cd0766518a290
pasberth <pasberth@gmail.com>
2012-08-26 21:24:27 +0900

    またいろいろ変更

--- src/sif/classes.py
+++ src/sif/classes.py
@@ -20,11 +20,10 @@ class SpriteImageFormat(object):
     def __init__(self, config=None):
         self.config = self.verify_config(config)
         self._is_loaded = False
-        self.build()
-        #super(SpriteImageFormat, self).__init__(*args, **kwargs)
+        self.load()
 
     def _default_outfile(self):
-        return "%s.%s" % (os.path.splitext(self.name)[1], self.outformat.lower())
+        return "%s.%s" % (os.path.splitext(self.path)[1], self.outformat.lower())
     
     def _default_outformat(self):
         fmt = self._get_image().format
@@ -43,6 +42,13 @@ class SpriteImageFormat(object):
             return self._default_outformat()
         else:
             return self.config["format"].lower()
+
+    @property
+    def path(self):
+        if not self.config.has_key("path"):
+            return ""
+        else:
+            return self.config["path"]
 
     @property
     def name(self):
@@ -92,49 +98,53 @@ class SpriteImageFormat(object):
         self._load()
         return True
     
-    def build(self, always_list=False):
-        ret = self._build()
+    def build(self, config=None, always_list=False):
+        ret = self._build(config=self.config if config is None else config)
         if always_list and not isinstance(ret, list):
             ret = [ret]
         if not ret:
             raise TypeError, "_build() should return [image] over than 1 elements"
         return ret
 
-    def _build(self):
-        self.load()
-        
+    def _build(self, config={}):
         base   = self._get_image()
         layers = self.layers
-        ret = self._build_with(base, layers)
+        cfg = {
+          "layers": [], # merge all layers.
+          "x": 0,
+          "y": 0,
+        }
+        ret = self._build_with(base, layers, dict(config, **cfg))
         if isinstance(ret, list) and len(ret) == 1:
             return ret[0]
         else:
             return ret
 
-    def _build_with(self, base, layers):
+    def _build_with(self, base, layers, config):
         if not layers:
-            return [base]
+            return [PlainSpriteImageFormat(base, dict(config))]
         layer = layers[-1]
         result = []
-        for over_image in layer.build(always_list=True):
+        for over in layer.build(config=config, always_list=True):
+            over_image = over.image
             _base = base.copy()
             over_image_width, over_image_height = over_image.size
-            over_image = over_image.resize( (layer.width or over_image_width, layer.height or over_image_height) )
-            #over_image = over_image.crop( (0, 0, layer.width or over_image_width, layer.height or over_image_height) )
+            over_image = over_image.resize( (layer.width or over_image_width,
+                                             layer.height or over_image_height) )
             bands = over_image.split()
             if len(bands) == 4:
                 alpha = bands[3]
                 _base.paste(over_image, (layer.x, layer.y), mask=alpha)
             else:
                 _base.paste(over_image, (layer.x, layer.y))
-            result += self._build_with(_base, layers[0:-1])
+            result += self._build_with(_base, layers[0:-1], over.config)
         return result
 
     def _create_canvas(self):
         return Image.new('RGBA', (self.width, self.height), (0, 0, 0, 0))
 
     def _get_image(self):
-        return self._create_canvas()        
+        return Image.new('RGBA', (0, 0), (0, 0, 0, 0))
 
     def has_config_param(self, param_key):
         return param_key in self.HAS_CONFIG_PARAMS
@@ -142,7 +152,8 @@ class SpriteImageFormat(object):
     def open_layer(self, config):
         config = self.verify_layer_config(config)
         if config["type"] == "sif":
-            return OpenSpriteImageFormat(config=config)
+            return OpenSpriteImageFormat(path=os.path.join(self.path, config["name"]),
+                                         config=config)
         elif config["type"] == "diff":
             return DiffSpriteImageFormat(config=config)
         else:
@@ -176,20 +187,22 @@ class SpriteImageFormat(object):
         else:
             raise TypeError, "config must be a dict"
         if config.has_key("name") and config["name"]:
-            config["name"] = os.path.join(self.name, config["name"])
+            config["path"] = os.path.join(self.path, config["name"])
         else:
-            config["name"] = self.name
+            config["path"] = self.path
         return config
         
 
-    def save(self, name=None, format=None):
-        
-        name = name or self.outfile
-        format = format or self.outformat
-        imgs = self.build(always_list=True)
+    def save(self, outfile=None, outformat=None):
+        if outfile is None:
+            outfile = self.outfile
+        if outformat is None:
+            outformat = self.outformat
+        imgs = self.build(config = { "outfile": outfile,
+                                     "outformat": outformat, },
+                          always_list=True)
         for img in imgs:
-            img.show()
-        img.save(name, format)
+            img.image.save(img.outfile, img.outformat)
         return True
 
 class NewFile(object):
@@ -203,69 +216,77 @@ class NewFile(object):
     def _load(self):
         return True
 
+class PlainFile(object):
+    
+    def __init__(self, image, config=None):
+        self._image = image
+        super(PlainFile, self).__init__(config=config)
+
+    @property
+    def image(self):
+        return self._get_image()
+
+    def _get_image(self):
+        return self._image
+    
+    def _load(self):
+        return True
+
 class OpenFile(object):
 
-    def __init__(self, name=None, config=None):
+    def __init__(self, path=None, config=None):
         config = config or {}
         config = config.copy()
-        if not name and not config or config and not config.has_key("name"):
-            raise TypeError, "Required argument 'name' (pos 1) not found"
-        name = name or config["name"]
-        if not os.path.exists(name):
-            raise IOError, "No such file or directory: '%s'" % (name,)
-        if not issif(name):
-            raise IOError, "Not a SIF file: '%s'" % (name,)
-        self.name = name
-        config["name"] = name
+        if not path and not config or config and not config.has_key("path"):
+            raise TypeError, "Required argument 'path' (pos 1) not found"
+        path = path or config["path"]
+        if not os.path.exists(path):
+            raise IOError, "No such file or directory: '%s'" % (path,)
+        if not issif(path):
+            raise IOError, "Not a SIF file: '%s'" % (path,)
+        config["path"] = path
         super(OpenFile, self).__init__(config=config)
-
-    # Reset @property
-    name = None
 
     @property
     def width(self):
         if not self.config.has_key("width"):
-            return self._get_image().size[0]
+            if issif_as_image(self.path):
+                return self._get_image().size[0]
+            else:
+                raise TypeError, "config should has 'width' property."
         else:
             return int(self.config["width"])
 
     @property
     def height(self):
         if not self.config.has_key("height"):
-            return self._get_image().size[1]
+            if issif_as_image(self.path):
+                return self._get_image().size[1]
+            else:
+                raise TypeError, "config should has 'height' property."
         else:
             return int(self.config["height"])
 
     def _get_image(self):
-        if issif_as_image(self.name):
-            return Image.open(self.name)
+        if issif_as_image(self.path):
+            return Image.open(self.path)
         else:
             return self._create_canvas()
 
     def _load(self):
-        if issif_as_image(self.name):
+        if issif_as_image(self.path):
             return self._load_as_image()
         else:
             return self._load_not_image()
 
     def _load_as_image(self):
-        self.image = self._get_image()
-        #width, height = self.image.size
-        self.config = {
-            "name": self.name,
-            "outfile": self.outfile,
-            "outformat": self.image.format,
-            "x": self.x,
-            "y": self.y,
-            "width": self.width,
-            "height": self.height,
-        }
+        return True
 
     def _load_not_image(self):
-        with open(cfgpath(self.name)) as f:
-            self.config = yaml.load(f.read())
+        with open(cfgpath(self.path)) as f:
+            self.config = dict(self.config, **yaml.load(f.read()))
             if not self.config.has_key("name"):
-                self.config["name"] = self.name
+                self.config["name"] = self.path
 
 class DiffFile(object):
     
@@ -278,11 +299,20 @@ class DiffFile(object):
     def _load(self):
         return True
 
-    def _build(self):
+    def _build(self, config={}):
         # todo: nested diff
-        return [layer.build() for layer in self.layers]
+        imgs = []
+        config = dict(self.config, **config)
+        for layer in self.layers:
+            cfg = { "outfile": os.path.join(config["outfile"], layer.name) }
+            img = layer.build(config=dict(config, **cfg))
+            imgs.append(img)
+        return imgs
 
 class NewSpriteImageFormat(NewFile, SpriteImageFormat):
+    pass
+
+class PlainSpriteImageFormat(PlainFile, SpriteImageFormat):
     pass
 
 class OpenSpriteImageFormat(OpenFile, SpriteImageFormat):
